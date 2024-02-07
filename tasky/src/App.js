52b5103503002225ed4437f0707631d3a017dc6c
Ciara Martin <ciaramartinn18@gmail.com>
2024-02-07 10:04:04 +0000

    Week 4 Lab A done

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import './App.css';
 import Task from './components/Task';
+import AddTaskForm from './components/Form';
+import { v4 as uuidv4 } from 'uuid';
+
 function App() {
   const [ taskState, setTaskState ] = useState({
     tasks: [
@@ -20,22 +23,61 @@ function App() {
     tasks.splice(taskIndex, 1);
     setTaskState({tasks});
   } 
+  const [ formState, setFormState ] = useState({
+    title: "",
+    description: "",
+    deadline: ""
+  });
+
+  const formChangeHandler = (event) => {
+    let form = {...formState};
+
+    switch(event.target.name) {
+      case "title":
+          form.title = event.target.value;
+          break;
+      case "description":
+          form.description = event.target.value;
+          break;
+      case "deadline":
+          form.deadline = event.target.value;
+          break;
+      default:
+          form = formState;
+    }
+    setFormState(form);
+  }
+  console.log(formState);
+  
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const tasks = [...taskState.tasks];
+    const form = {...formState};
+
+    form.id = uuidv4();
+    
+    tasks.push(form);
+    setTaskState({tasks});
+  }
 
   return (
     <div className="container">
-      <h1>Tasky</h1>
-      {taskState.tasks.map((task, index) => (              
-    <Task 
-      title={task.title}
-      description={task.description}
-      deadline={task.deadline}
-      key={task.id}
-      done={task.done}
-      markDone={() => doneHandler(index)}
-      deleteTask = {() => deleteHandler(index)}
-    />
-  ))}
-    </div>
+    <h1>Tasky</h1>
+    {taskState.tasks.map((task, index) => (              
+      <Task 
+        title={task.title}
+        description={task.description}
+        deadline={task.deadline}
+        key={task.id}
+        done={task.done}
+        markDone={() => doneHandler(index)}
+        deleteTask = {() => deleteHandler(index)}
+      />
+    ))}
+    <AddTaskForm submit={formSubmitHandler} change={formChangeHandler} />
+    
+  </div>
   );
 }
 
